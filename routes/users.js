d0f2ff11b6f0665f4258abef3081775e77f36e11
Tyler Boromeo <boromeotyler@gmail.com>
2021-08-23 16:34:32 -0700

    Created utils.js, implemented restoreUser(), added csrf and async hanlder in user rotues

--- routes/users.js
+++ routes/users.js
@@ -3,27 +3,32 @@ var router = express.Router();
 let bcrypt = require("bcryptjs")
 let {User} = require("../db/models")
 let loginUser = require("../auth.js")
-const {check,validationResult} = require("express-validator")
+const {check, validationResult} = require("express-validator")
+const { csrfProtection, asyncHandler } = require('./utils')
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+router.get('/', function(req, res) {
   res.send('respond with a resource');
 });
 
-router.get("/signup",async(req,res,next)=>{
-  res.render("sign-up")
-})
+router.get("/signup", csrfProtection, (req, res)=>{
+  res.render("sign-up", {
+    csrfToken: req.csrfToken(),
+  });
+});
 
-router.post("/signup",async(req,res,next)=>{
+router.post("/signup", csrfProtection, asyncHandler(async(req,res,next)=>{
   let {username,password,email,confirmPassword} = req.body
   if(password!==confirmPassword){
-    return res.render("sign-up")
+    return res.render("sign-up", {
+      csrfToken: req.csrfToken(),
+    });
   }
   let hashedPassword = await bcrypt.hash(password,10)
   let user = await User.create({username,hashedPassword,email})
   loginUser(req,res,user)
   res.redirect("/")
-})
+}));
 
 
 module.exports = router;
