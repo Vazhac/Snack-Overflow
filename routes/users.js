6f48728bd8bfc7cf9886ce4212c968c8e24b38ec
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-08-23 15:35:08 -0700

    added sign-up authentication

--- routes/users.js
+++ routes/users.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 let bcrypt = require("bcryptjs")
 let {User} = require("../db/models")
+let loginUser = require("../auth.js")
+const {check,validationResult} = require("express-validator")
 
 /* GET users listing. */
 router.get('/', function(req, res, next) {
@@ -13,9 +15,13 @@ router.get("/signup",async(req,res,next)=>{
 })
 
 router.post("/signup",async(req,res,next)=>{
-  let {username,password,email} = req.body
-  let hashedPassword = bcrypt.hash(password,10)
+  let {username,password,email,confirmPassword} = req.body
+  if(password!==confirmPassword){
+    return res.render("sign-up")
+  }
+  let hashedPassword = await bcrypt.hash(password,10)
   let user = await User.create({username,hashedPassword,email})
+  loginUser(req,res,user)
   res.redirect("/")
 })
 
