1cffdab8f931b8bfd96e78280e353afcca11d01b
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-08-23 14:35:26 -0700

    fixed models issues user has many association and sequelize/datatype

--- db/models/user.js
+++ db/models/user.js
@@ -22,7 +22,7 @@ module.exports = (sequelize, DataTypes) => {
   );
   User.associate = function (models) {
     // associations can be defined here
-    User.hasMany(models.Post, {
+    User.hasMany(models.Answer, {
       foreignKey: "userId",
     });
     User.hasMany(models.Comment, {
