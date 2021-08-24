3cbaf6fe164782c89638aac74a53e2b4a28fe759
Tyler Boromeo <boromeotyler@gmail.com>
2021-08-24 09:26:06 -0700

    implemented signup post route and coressponding pug view

--- routes/users.js
+++ routes/users.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 let bcrypt = require("bcryptjs")
-let {User} = require("../db/models")
-let loginUser = require("../auth.js")
-const {check, validationResult} = require("express-validator")
+let { User } = require("../db/models")
+let {loginUser, logoutUser} = require("../auth.js")
+const {validationResult } = require("express-validator")
 const { csrfProtection, asyncHandler } = require('./utils')
+const { signInValidators, signUpValidators } = require('../validators'); //Possibly add more comple validations for checking password complexity and confirm password complexictyu
 
 /* GET users listing. */
 router.get('/', function(req, res) {
@@ -12,22 +13,38 @@ router.get('/', function(req, res) {
 });
 
 router.get("/signup", csrfProtection, (req, res)=>{
+  const errors = [];
   res.render("sign-up", {
     csrfToken: req.csrfToken(),
+    errors,
   });
 });
 
-router.post("/signup", csrfProtection, asyncHandler(async(req,res,next)=>{
-  let {username, password, email, confirmPassword} = req.body
-  if(password!==confirmPassword){
-    return res.render("sign-up", {
+router.post("/signup", csrfProtection, signUpValidators, asyncHandler(async(req, res)=>{
+  let {username, password, email} = req.body
+  const validatorErrors = validationResult(req);
+
+  const user = await User.build({
+    username,
+    email
+  });
+
+  if (validatorErrors.isEmpty()) {
+    let hashedPassword = await bcrypt.hash(password, 10)
+    user.hashedPassword = hashedPassword;
+    user.save();
+    loginUser(req, res, user);
+    res.redirect("/")
+  } else {
+    const errors = validatorErrors.array().map((err) => err.msg);
+    res.render('sign-up', {
+      title: 'Sign-up',
+      user,
+      errors,
       csrfToken: req.csrfToken(),
     });
   }
-  let hashedPassword = await bcrypt.hash(password,10)
-  let user = await User.create({username,hashedPassword,email})
-  loginUser(req,res,user)
-  res.redirect("/")
+
 }));
 
 router.get("/signin", csrfProtection, asyncHandler(async(req, res, next) => {
