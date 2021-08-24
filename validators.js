e5e59f8b8e7abffb3dcd4cef071aab5eb11b3e00
Tyler Boromeo <boromeotyler@gmail.com>
2021-08-24 08:45:53 -0700

    Added validator functions and associated file for them

---
+++ validators.js
@@ -0,0 +1,38 @@
+const signUpValidators = [
+  check('username')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Username')
+    .isLength({ max: 50 })
+    .withMessage('Username must not be more than 50 characters long'),
+  check('emailAddress')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Email Address')
+    .isLength({ max: 255 })
+    .withMessage('Email Address must not be more than 255 characters long')
+    .isEmail()
+    .withMessage('Email Address is not a valid email'),
+  check('password')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Password')
+    .isLength({ max: 255 })
+    .withMessage('Password must not be more than 255 characters long'),
+  check('confirmPassword')
+    .exists({ checkFalsy: true })
+    .withMessage('Please provide a value for Confirm Password')
+    .isLength({ max: 255 })
+    .withMessage('Confirm Password must not be more than 255 characters long'),
+];
+
+const signInValidators = [
+  check('username')
+  .exists({ checkFalsy: true })
+  .withMessage('Please provide a value for Username'),
+  check('password')
+  .exists({ checkFalsy: true })
+  .withMessage('Please provide a value for Password'),
+]
+
+module.exports = {
+  signUpValidators,
+  signInValidators
+}
