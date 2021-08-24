a1ec2b79ef745d517d89900f8660ba40a277792c
Andres <aecheverri43@gmail.com>
2021-08-24 14:48:18 -0700

    Add question page pug, edit/delete question form

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,3 +1,33 @@
-window.addEventListener("load", (event)=>{
-    console.log("hello from javascript!")
-})
+global.fetch = require("node-fetch");
+
+window.addEventListener("load", (event) => {
+  console.log("hello from javascript!");
+  let deleteButton = document.getElementsByClassName("delete")[0];
+  let editButton = document.getElementsByClassName("edit")[0];
+  let editForm = document.getElementById("editForm");
+  let submitButton = document.getElementById("submit");
+
+  deleteButton.addEventListener("click", async (event) => {
+    let res = await fetch(`localhost:8080/questions/${deleteButton.id}`, {
+      method: "delete",
+    });
+  });
+
+  editButton.addEventListener("click", async (event) => {
+    console.log("edit button event listener is working");
+    editForm.style.display = "block";
+  });
+
+  submitButton.addEventListener("click", async (event) => {
+    event.preventDefault();
+    let title = editForm.children[0];
+    let message = editForm.children[1];
+
+    let res = await fetch(`localhost:8080/questions/${editButton.id}`, {
+      method: "put",
+      body: { title: title.value, message: message.value },
+    });
+
+    editForm.style.display = "none";
+  });
+});
