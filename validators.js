a1a6a8a83ee920891806ce0f80f38d5d7663d867
Tyler Boromeo <boromeotyler@gmail.com>
2021-08-24 09:03:56 -0700

    Added additional checks for email, username, and password

--- validators.js
+++ validators.js
@@ -1,16 +1,35 @@
+const { check } = require('express-validator');
+const db = require('./db/models');
+
 const signUpValidators = [
   check('username')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Username')
     .isLength({ max: 50 })
-    .withMessage('Username must not be more than 50 characters long'),
+    .withMessage('Username must not be more than 50 characters long')
+    .custom((value) => {
+      return db.User.findOne({ where: { username: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided username is already in use by another account');
+          }
+        });
+    }),
   check('email')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Email Address')
     .isLength({ max: 255 })
     .withMessage('Email Address must not be more than 255 characters long')
     .isEmail()
-    .withMessage('Email Address is not a valid email'),
+    .withMessage('Email Address is not a valid email')
+    .custom((value) => {
+      return db.User.findOne({ where: { email: value } })
+        .then((user) => {
+          if (user) {
+            return Promise.reject('The provided Email Address is already in use by another account');
+          }
+        });
+    }),
   check('password')
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Password')
@@ -20,7 +39,13 @@ const signUpValidators = [
     .exists({ checkFalsy: true })
     .withMessage('Please provide a value for Confirm Password')
     .isLength({ max: 255 })
-    .withMessage('Confirm Password must not be more than 255 characters long'),
+    .withMessage('Confirm Password must not be more than 255 characters long')
+    .custom((value, { req }) => {
+      if (value !== req.body.password) {
+        throw new Error('Confirm Password does not match Password');
+      }
+      return true;
+    })
 ];
 
 const signInValidators = [
