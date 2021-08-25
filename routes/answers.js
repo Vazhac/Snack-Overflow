3af3d1eae3aa0f166a22e0e6e82d393744593494
Andres <aecheverri43@gmail.com>
2021-08-25 09:47:26 -0700

    Add answer comment routes, answer and comment delete/edit buttons

---
+++ routes/answers.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();
+let { User, Question, Answer, Comment } = require("../db/models")
+
+const { validationResult } = require("express-validator")
+const { csrfProtection, asyncHandler } = require('./utils')
+const { questionValidators, replyValidators} = require('../validators'); 
+
+router.delete("/:id", asyncHandler(async (req, res) => {
+    let answer = await Answer.findByPk(req.params.id);
+    await answer.destroy();
+}));
+
+
+module.exports = router;
