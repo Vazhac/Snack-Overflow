a431562c3e45820e2237fd151ed4beb300e11a7f
Stevenbarnett1 <Stevenbarnett1@live.com>
2021-08-25 16:24:53 -0700

    all answer/comment edit/delete functionality finished

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,12 +1,12 @@
 window.addEventListener("load", (event) => {
   console.log("hello from javascript!");
   let deleteButton = document.getElementsByClassName("delete-question")[0];
-  let editButton = document.getElementsByClassName("edit-question")[0];
+  let editButton = document.getElementsByClassName("edit-question-button")[0];
   let answerButton = document.getElementsByClassName("answer-question")[0];
   let commentButton = document.getElementsByClassName("comment-question")[0];
-  let editForm = document.getElementById("editForm");
-  let answerForm = document.getElementById("answerForm");
-  let commentForm = document.getElementById("commentForm");
+  let editForm = document.getElementById("edit-form");
+  let answerForm = document.getElementById("answer-form");
+  let commentForm = document.getElementById("comment-form");
   let editSubmitButton = document.getElementById("edit-submit");
   let answerSubmitButton = document.getElementById("answer-submit");
   let commentSubmitButton = document.getElementById("comment-submit");
@@ -14,6 +14,16 @@ window.addEventListener("load", (event) => {
   let answerDeleteButton = document.getElementsByClassName("delete-answer");
   let commentEditButton = document.getElementsByClassName("edit-comment");
   let commentDeleteButton = document.getElementsByClassName("delete-comment");
+  let editCommentForm = document.querySelector("#edit-comment-form")
+  let editAnswerForm = document.querySelector("#edit-answer-form")
+  let editAnswerSubmit = document.querySelector("#edit-answer-submit")
+  let editCommentSubmit = document.querySelector("#edit-comment-submit")
+
+
+
+  editButton.addEventListener("click", async (event) => {
+    editForm.style.display = "block";
+  });
 
   deleteButton.addEventListener("click", async (event) => {
     let id = Number(deleteButton.id.slice(15))
@@ -26,10 +36,78 @@ window.addEventListener("load", (event) => {
 
     window.location = `http://localhost:8080`;
   });
+  let addEventListenerToEditAnswerForm = async (editAnswerSubmit) => {
+  if(editAnswerSubmit){
+    editAnswerSubmit.addEventListener("click",async event => {
+      event.preventDefault()
+      let errors = document.getElementById("errors");
+      if (errors.innerHTML) {
+        errors.innerHTML = "";
+      }
+      let message = editAnswerForm.children[0].value
+      let res = await fetch(`http://localhost:8080/answers/${editAnswerForm.getAttribute('answerId')}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "put",
+        body:JSON.stringify({message})
+      });
+      res = await res.json()
+      if(res.message){
+        document.getElementById(`answer-id${res.id}`).innerText = res.message;
+        editAnswerForm.style.display = "none";
+      } else {
+        for (let error of res) {
+          let li = document.createElement("li");
+          li.innerText = error;
+          errors.append(li);
+        }
+      }
+    })
+  }
+}
+let addEventListenerToEditCommentForm = async (editCommentSubmit) => {
+  console.log("EDIT COMMENT SUBMIT",editCommentSubmit)
+  if (editCommentSubmit){
+    console.log("EDIT COMMENT SUBMIT",editCommentSubmit)
+    editCommentSubmit.addEventListener("click",async event => {
+      event.preventDefault()
+      let errors = document.getElementById("errors");
+      if (errors.innerHTML) {
+        errors.innerHTML = "";
+      }
+      let message = editCommentForm.children[0].value
+      let res = await fetch(`http://localhost:8080/comments/${editCommentForm.getAttribute('commentId')}`, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+        method: "put",
+        body:JSON.stringify({message})
+      });
+      res = await res.json()
+      if(res.message){
+        document.getElementById(`comment-id${res.id}`).innerText = res.message;
+        editCommentForm.style.display = "none";
+      } else {
+        for (let error of res) {
+          let li = document.createElement("li");
+          li.innerText = error;
+          errors.append(li);
+        }
+      }
+    })
+  }
+}
 
   if (answerEditButton.length) {
     for (editButton of answerEditButton) {
-      editButton.addEventListener("click", async (event) => {});
+      editButton.addEventListener("click", async (event) => {
+       editAnswerForm.style.display = "block"
+       editAnswerForm.setAttribute('answerId',`${Number(editButton.id.slice(12))}`)
+       console.log("ANSWER EDIT BUTTON: ",editButton.id.slice(12))
+       console.log(editButton.id.slice(12))
+       console.log("edit answer form answer id attribute: ", editAnswerForm.getAttribute('answerId'))
+      });
     }
   }
 
@@ -49,9 +127,14 @@ window.addEventListener("load", (event) => {
 
   if (commentEditButton.length) {
     for (editButton of commentEditButton) {
-      editButton.addEventListener("click", async (event) => {});
+      editButton.addEventListener("click", async (event) => {
+        editCommentForm.style.display = "block"
+        editCommentForm.setAttribute('commentId',`${Number(editButton.id.slice(12))}`)
+      });
     }
   }
+
+
 
   if (commentDeleteButton.length) {
     for (deleteButton of commentDeleteButton) {
@@ -68,11 +151,6 @@ window.addEventListener("load", (event) => {
     }
   }
 
-
-
-  editButton.addEventListener("click", async (event) => {
-    editForm.style.display = "block";
-  });
 
   commentButton.addEventListener("click", async (event) => {
     commentForm.style.display = "block";
@@ -116,17 +194,19 @@ window.addEventListener("load", (event) => {
       li.append(editButton);
       document.querySelector("ul.comments").append(li);
       commentForm.style.display = "none";
-
       deleteButton.addEventListener("click", async (event) => {
         await fetch(`http://localhost:8080/comments/${res.id}`, {
           method: "delete",
         });
-
-        // li.remove();
         let comment = document.querySelector(`#comment-id${res.id}`)
         console.log(comment)
         comment.remove();
       })
+      editButton.addEventListener("click",async event => {
+        editCommentForm.style.display = "block"
+        editCommentForm.setAttribute('commentId',`${Number(editButton.id.slice(12))}`)
+      })
+      addEventListenerToEditCommentForm(editCommentForm.children[1])
     } else {
       for (let error of res) {
         let li = document.createElement("li");
@@ -180,15 +260,20 @@ window.addEventListener("load", (event) => {
       answerForm.style.display = "none";
 
       deleteButton.addEventListener("click", async (event) => {
-        // console.log(res);
         await fetch(`http://localhost:8080/answers/${res.id}`, {
           method: "delete",
         });
-        // li.remove();
         let answer = document.querySelector(`#answer-id${res.id}`)
         console.log(answer)
         answer.remove();
       })
+
+      editButton.addEventListener("click",async event => {
+        editAnswerForm.style.display = "block"
+        editAnswerForm.setAttribute('answerId',`${Number(editButton.id.slice(11))}`)
+        console.log("YEAHH: ",editButton.id,"   ", Number(editButton.id.slice(11)))
+      })
+      addEventListenerToEditAnswerForm(editAnswerForm.children[1])
     } else {
       for (let error of res) {
         let li = document.createElement("li");
@@ -226,4 +311,6 @@ window.addEventListener("load", (event) => {
       }
     }
   });
+  addEventListenerToEditAnswerForm(editAnswerSubmit)
+addEventListenerToEditCommentForm(editCommentSubmit)
 });
